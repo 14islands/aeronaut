7729226e03d4e6a3a714d33752a1475392c72721
David Lindkvist <david@14islands.com>
2020-04-01 09:52:18 +0200

    fix build deps

---
+++ libs.sh
@@ -0,0 +1,3 @@
+cp node_modules/three/build/three.js build/three.js
+cp node_modules/webvr-polyfill/build/webvr-polyfill.js build/webvr-polyfill.js
+cp node_modules/webvr-boilerplate/build/webvr-manager.js build/webvr-manager.js
